d900729c59bbedb85b708b1c1539dd04a1fcbf72
Samantha <samanthavillafana8@gmail.com>
2024-04-27 15:01:13 -0400

    complete lab

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,15 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 
-function PlantCard() {
+function PlantCard({plant}) {
+  const { name, image, price } = plant
+  const [ inStock, setStock ] = useState(true)
+
+  function handleClick(){
+    setStock((inStock) => !inStock)
+  }
   return (
     <li className="card">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      <img src={image} alt={name} />
+      <h4>{name}</h4>
+      <p>Price: {price}</p>
+      {inStock ? (
+        <button onClick={handleClick}className="primary">In Stock</button>
       ) : (
-        <button>Out of Stock</button>
+        <button onClick={handleClick}>Out of Stock</button>
       )}
     </li>
   );
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [ plants, setPlants] = useState([])
+  const [ searchTerm, setSearchTerm] = useState("")
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+    .then((r) => r.json())
+    .then((data) => setPlants(data))
+  }, [])
   return (
     <main>
       <NewPlantForm />
-      <Search />
-      <PlantList />
+      <Search setSearchTerm={setSearchTerm}/>
+      {<PlantList plants = {plants} searchTerm={searchTerm}/>}
     </main>
   );
 }
